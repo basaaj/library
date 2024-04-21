4276202fb0b018e24323271cbc2e1d379a29775b
j <93398891+basaaj@users.noreply.github.com>
2024-04-21 18:32:16 -0500

    Display books if any in array

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const bookTitle = document.querySelector('#title');
 const bookAuthor = document.querySelector('#author');
 const bookPages = document.querySelector('#pages');
 const bookRead = document.querySelector('#read');
+const books = document.querySelector('.books');
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -15,4 +16,18 @@ function Book(title, author, pages, read) {
 function addBookToLibrary() {
     let newBook = new Book(bookTitle.textContent, bookAuthor.textContent, bookPages.textContent, bookRead.value);
     myLibrary.push(newBook);
+}
+
+function displayBook(book) {
+    let newDiv = document.createElement('div');
+    newDiv.className = 'book';
+    newDiv.textContent = book.title + book.author + book.pages + book.read;
+    books.appendChild(newDiv);
+}
+
+myLibrary.push(new Book("Naruto ", "Masashi Kishimoto ", 250, true));
+myLibrary.push(new Book("Bleach ", "Tite Kubo ", 250, true));
+
+for (i = 0; i < myLibrary.length; i++) {
+    displayBook(myLibrary[i]);
 }
